605f294b19274e4c029f88378669a5fdc0aa24e4
Sagar Khatri <khatrisagar2@gmail.com>
2024-07-03 17:39:34 +0530

    Login Form Java

--- app/src/main/java/internship/june/MainActivity.java
+++ app/src/main/java/internship/june/MainActivity.java
@@ -1,18 +1,53 @@
 package internship.june;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button signin;
+    EditText email,password;
+
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signin = findViewById(R.id.main_signin);
+        email = findViewById(R.id.main_email);
+        password = findViewById(R.id.main_password);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(email.getText().toString().trim().equals("")){
+                    email.setError("Email Id Required");
+                }
+                else if(!email.getText().toString().trim().matches(emailPattern)){
+                    email.setError("Valid Email Id Required");
+                }
+                else if(password.getText().toString().trim().equals("")){
+                    password.setError("Password Required");
+                }
+                else if(password.getText().toString().trim().length()<6){
+                    password.setError("Min. 6 Char Password Required");
+                }
+                else {
+                    System.out.println("Login Successfully");
+                    Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                    Snackbar.make(view, "Login Successfully", Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 }
